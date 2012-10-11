1b79f1d616b45ff1778bd1cf5e82bd07a4c9348d
Luis Fung <fungl164@hotmail.com>
2012-10-11 06:01:26 -0400

    fixes for new_sub_policy model and spec tests

--- app/models/new_sub_policy.rb
+++ app/models/new_sub_policy.rb
@@ -7,7 +7,7 @@ class NewSubPolicy
   embedded_in :policy, class_name: 'NewPolicy'
   embeds_many :attributes, class_name: 'NewSubPolicyAttribute'
 
-  validates_presence_of :name, on: [:create, :update]  
+  validates_presence_of :name  
   validates_format_of :name, with: /\A\w+\Z/
 
   attr_accessible :name, :attributes
---
+++ spec/models/new_sub_policy_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe NewSubPolicy do
+  it { should have_fields(:name).of_type(String) }
+  it { should be_embeded_in(:policy) } # .of_type(NewPolicy)
+  it { should embed_many(:attributes).of_type(NewSubPolicyAttribute) }
+
+  it { should allow_mass_assignment_of(:name) }
+  it { should allow_mass_assignment_of(:attributes) }
+
+  it { should validate_presence_of(:name).on([:create, :update]) }
+  it { should validate_format_of(:name).to_allow("myname").not_to_allow("$my name!") }
+
+  it { should be_timestamped_document } 
+end

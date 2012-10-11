56904d002692570c080b240d4f4ddf0aeb604b6c
Luis Fung <fungl164@hotmail.com>
2012-10-11 15:00:56 -0400

    modifications to model validations for policies and subpolicies

--- spec/models/new_sub_policy_spec.rb
+++ spec/models/new_sub_policy_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe NewSubPolicy do
   it { should have_fields(:name).of_type(String) }
-  it { should be_embeded_in(:policy) } # .of_type(NewPolicy)
+  it { should be_embedded_in(:policy) } # .of_type(NewPolicy)
   it { should embed_many(:attributes).of_type(NewSubPolicyAttribute) }
 
   it { should allow_mass_assignment_of(:name) }
@@ -12,4 +12,11 @@ describe NewSubPolicy do
   it { should validate_format_of(:name).to_allow("myname").not_to_allow("$my name!") }
 
   it { should be_timestamped_document } 
+
+  it "accepts nested attributes" do
+    atributes_attributes = %w{ val1 val2 val3 }.each do
+      
+      end
+
+  end
 end

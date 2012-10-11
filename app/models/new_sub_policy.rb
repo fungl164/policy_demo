367949bd936296ec847879fc936e311958959f63
Luis Fung <fungl164@hotmail.com>
2012-10-11 16:09:01 -0400

    added shoulda gist to validate nested attributes in models

--- app/models/new_sub_policy.rb
+++ app/models/new_sub_policy.rb
@@ -4,12 +4,24 @@ class NewSubPolicy
 
   field :name, type: String
 
+  has_one :policy_domain, class_name: 'NewPolicyDomain'
   embedded_in :policy, class_name: 'NewPolicy'
   embeds_many :attributes, class_name: 'NewSubPolicyAttribute'
 
   validates :name, :presence => true, :uniqueness => true, :format => { :with => /^[\w\-]+$/ }
 
-  attr_accessible :name, :attributes
+  attr_accessible :name, :policy, :policy_domain, :attributes
 
-  accepts_nested_attributes_for :policy, :attributes
+  accepts_nested_attributes_for :policy, :policy_domain, :attributes
+end
+
+class NewPolicyDomain
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :name, type: String
+
+  validates :name, :presence => true, :uniqueness => true, :format => { :with => /^[\w\-]+$/ }
+
+  attr_accessible :name
 end
--- spec/models/new_sub_policy_spec.rb
+++ spec/models/new_sub_policy_spec.rb
@@ -2,21 +2,43 @@ require 'spec_helper'
 
 describe NewSubPolicy do
   it { should have_fields(:name).of_type(String) }
+
+  it { should have_one(:policy_domain) } 
   it { should be_embedded_in(:policy) } # .of_type(NewPolicy)
   it { should embed_many(:attributes).of_type(NewSubPolicyAttribute) }
 
   it { should allow_mass_assignment_of(:name) }
+  it { should allow_mass_assignment_of(:policy) }
+  it { should allow_mass_assignment_of(:policy_domain) }
   it { should allow_mass_assignment_of(:attributes) }
 
   it { should validate_presence_of(:name).on([:create, :update]) }
+  it { should validate_uniqueness_of(:name) }
   it { should validate_format_of(:name).to_allow("myname").not_to_allow("$my name!") }
 
   it { should be_timestamped_document } 
 
+  it { should accept_nested_attributes_for :policy }
+  it { should accept_nested_attributes_for :policy_domain }
+  it { should accept_nested_attributes_for :attributes }
+
   it "accepts nested attributes" do
     atributes_attributes = %w{ val1 val2 val3 }.each do
       
-      end
-
+    end
   end
+
+end
+
+describe NewPolicyDomain do
+  it { should have_fields(:name).of_type(String) }
+
+  it { should allow_mass_assignment_of(:name) }
+
+  it { should validate_presence_of(:name).on([:create, :update]) }
+  it { should validate_uniqueness_of(:name) }
+  it { should validate_format_of(:name).to_allow("myname").not_to_allow("$my name!") }
+
+  it { should be_timestamped_document } 
+
 end
